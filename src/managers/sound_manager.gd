126c4c03295d193a658fd0cae9f31a67688787b2
Greg <grzegorz.niespodzianek@gmail.com>
2026-03-27 18:24:17 +0100

    music added

--- src/managers/sound_manager.gd
+++ src/managers/sound_manager.gd
@@ -1,9 +1,27 @@
 extends Node
 
+var elevator: Sound = preload("res://const_data/sounds/elevator.tres")
+
 var select: Sound = preload("res://const_data/sounds/select.tres")
+
+var main_music_player: AudioStreamPlayer
+
+func _ready():
+	main_music_player = AudioStreamPlayer.new()
+	main_music_player.bus = "Music"
+	main_music_player.autoplay = true
+	add_child(main_music_player)
+	main_music_player.stream = elevator.audio_stream
+	main_music_player.finished.connect(_loop_music)
+	_loop_music()
+	
+func _loop_music():
+	main_music_player.play()
+	
 
 func play_sound(sound: Sound):
 	var audio_player = AudioStreamPlayer.new()
+	audio_player.bus = "SFX"
 	add_child(audio_player)
 	audio_player.stream = sound.audio_stream
 	audio_player.play()
