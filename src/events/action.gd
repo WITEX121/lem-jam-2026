939e069c753ce6819a65c9c535218b740925752f
Truyka <mmilewski2202@gmail.com>
2026-03-27 17:29:42 +0100

    Nameless function

--- src/events/action.gd
+++ src/events/action.gd
@@ -1,7 +1,7 @@
 class_name Action extends Node
 
 static var actions: Dictionary = {
-    "*REMOVE_EMPLOYEE": func test():
+    "*REMOVE_EMPLOYEE": func():
         print("Kasujemy klienta!")
         pass
 }
