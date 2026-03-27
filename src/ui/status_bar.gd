36899e3b7783da7bd2981c19fb3eb159f07ba9f9
wkrynski <wkrynski05@gmail.com>
2026-03-27 08:07:15 +0100

    Prettiefied status bar further

--- src/ui/status_bar.gd
+++ src/ui/status_bar.gd
@@ -2,6 +2,14 @@ extends PanelContainer
 
 @onready var progress_bar = $MarginContainer/HBoxContainer/ProgressBar
 @onready var progress_bar_stylebox: StyleBoxFlat = progress_bar.get_theme_stylebox("fill")
+@onready var label = $MarginContainer/HBoxContainer/Label
+
+@export var label_text: String = ""
+
+
+func _ready() -> void:
+	label.text = label_text
+	
 
 func _on_progress_bar_value_changed(value: float) -> void:
 	progress_bar_stylebox.bg_color = Color(1, 1, value/100)
