b2dcffb94f3fc63394a4a1960cde50d849399d99
KarolSiluch <siluchkarol@gmail.com>
2026-03-27 02:15:19 +0100

    Add reply generator

--- const_data/prompt_reply/reply_list.gd
+++ const_data/prompt_reply/reply_list.gd
@@ -4,3 +4,6 @@ class_name ReplyList
 @export var starts: Array[ReplyElement]
 @export var fillers: Array[ReplyElement]
 @export var ends: Array[ReplyElement]
+
+func random_reply() -> PromptReply:
+    return PromptReply.new(starts.pick_random(), fillers.pick_random(), ends.pick_random())
