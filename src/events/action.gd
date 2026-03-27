03ac78dd1b12dedfa9ce418819d716b688e262d1
Truyka <mmilewski2202@gmail.com>
2026-03-27 19:32:01 +0100

    Dziala

--- src/events/action.gd
+++ src/events/action.gd
@@ -4,5 +4,8 @@ static var actions: Dictionary = {
 	"*REMOVE_EMPLOYEE": func():
 		GameManager.fire_employee()
 		print("Kasujemy klienta!")
+		pass,
+	"*ADD_BASE_EVENTS": func():
+		EventManager.unlock_events_shuffle(Consts.BASE_EVENTS)
 		pass
 }
