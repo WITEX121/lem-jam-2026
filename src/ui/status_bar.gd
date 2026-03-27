714a1bbc0409f1c01dcd78f4895082865ef0f17a
wkrynski <wkrynski05@gmail.com>
2026-03-27 08:33:54 +0100

    Made progress bars prettier

--- src/ui/status_bar.gd
+++ src/ui/status_bar.gd
@@ -1,15 +1,31 @@
 extends PanelContainer
 
 @onready var progress_bar = $MarginContainer/HBoxContainer/ProgressBar
-@onready var progress_bar_stylebox: StyleBoxFlat = progress_bar.get_theme_stylebox("fill")
+@onready var progress_bar_stylebox: StyleBoxFlat = progress_bar.get_theme_stylebox("fill").duplicate()
 @onready var label = $MarginContainer/HBoxContainer/Label
 
 @export var label_text: String = ""
 
-
 func _ready() -> void:
 	label.text = label_text
-	
+
+func _process(delta: float) -> void:
+	if Input.is_key_pressed(KEY_0):
+		progress_bar.value = randf_range(0, 100)
 
 func _on_progress_bar_value_changed(value: float) -> void:
-	progress_bar_stylebox.bg_color = Color(1, 1, value/100)
+	var red = Color("E3170A")
+	var yellow = Color("E3DC0A")
+	var green = Color("39E30A")
+	var target = Color("FFFFFF")
+
+	value = clamp(value, 0.0, 100.0)
+	if value <= 50.0:
+		var weight = value / 50.0
+		target = red.lerp(yellow, weight)
+	else:
+		var weight = (value - 50.0) / 50.0
+		target = yellow.lerp(green, weight)
+
+	progress_bar_stylebox.bg_color = target
+	progress_bar.add_theme_stylebox_override("fill", progress_bar_stylebox)
