83c6ad645d31eced1831b6d5b09f50b3fcd346fe
Truyka <mmilewski2202@gmail.com>
2026-03-27 02:17:38 +0100

    Events testing and implementation

--- src/managers/game_manager.gd
+++ src/managers/game_manager.gd
@@ -8,5 +8,8 @@ var EVENTS: Array:
 	get: return _events
 
 func game_start():
+	for i in range(10):
+		events_manager.push_event_random_place(EVENTS.pick_random())
+
 	events_manager.push_event_front(EVENTS[Event.EventId.START_EVENT])
 	events_manager.pop_back_to_current()
