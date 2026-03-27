bc58d0af22beb5302446beb9722b747d7a53821b
Greg <grzegorz.niespodzianek@gmail.com>
2026-03-27 11:06:27 +0100

    answer_button added

---
+++ src/answer_button/answer_button.gd
@@ -0,0 +1,47 @@
+class_name AnswerButton extends Button
+
+
+@export var _normal_color: Color
+@export var _hover_color: Color
+@export var reply_element: ReplyElement
+
+@onready var _reply_label: Label = %ReplyLabel
+@onready var _background: PanelContainer = %Background
+
+
+var _stylebox: StyleBoxFlat
+var _current_background_color: Color
+
+var _is_mouse_on_top: bool = false
+
+func _ready():
+	_current_background_color = _normal_color
+	_reply_label.text = reply_element.text
+	_stylebox = _background.get_theme_stylebox("panel")
+	_stylebox.bg_color = _current_background_color
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+	pressed.connect(_on_pressed)
+
+
+func _process(delta):
+	if _is_mouse_on_top:
+		_current_background_color = _current_background_color.lerp(_hover_color, 0.4)
+		Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+	else:
+		_current_background_color = _current_background_color.lerp(_normal_color, 0.4)
+		Input.set_default_cursor_shape(Input.CURSOR_CROSS)
+	
+	_stylebox.bg_color = _current_background_color
+
+
+func _on_mouse_entered():
+	_is_mouse_on_top = true
+
+func _on_mouse_exited():
+	_is_mouse_on_top = false
+
+func _on_pressed():
+	# TODO: Pass info to GameManager about selection
+	pass
+	print("Selected: " + reply_element.text)
