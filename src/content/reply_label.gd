d7d9960c924825d6686a5de4ca178a224a9ba33c
KarolSiluch <siluchkarol@gmail.com>
2026-03-27 16:44:20 +0100

    .

---
+++ src/content/reply_label.gd
@@ -0,0 +1,11 @@
+extends Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	ReplyManager.text_on_change.connect(_on_change)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _on_change(reply: String):
+	text = reply
