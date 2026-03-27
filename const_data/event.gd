8c94939d86e6ab04ddf702796a12453e53db11b4
Truyka <mmilewski2202@gmail.com>
2026-03-27 01:50:57 +0100

    Events implementation

--- const_data/event.gd
+++ const_data/event.gd
@@ -1,4 +1,15 @@
 class_name Event extends Resource
 
 enum EventId {
+	START_EVENT
 }
+
+enum Type { MONEY = 0, MORALITY = 1, TRUST = 2, PR = 3 }
+
+@export var text: String = ""
+
+@export var affirmation_effects: Array[float] = [0, 0, 0, 0];
+@export var refutation_effects: Array[float] = [0, 0, 0, 0];
+
+@export var affirmation_events: Array[Event.EventId] = []
+@export var refutation_events: Array[Event.EventId] = []
---
+++ src/managers/prompt_screen.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@onready var sentence_begin = %SentenceBegin
+@onready var sentence_fill = %SentenceFill
+@onready var sentence_end = %SentenceEnd
+
+@onready var user_prompt = %UserPrompt
+
+func _ready() -> void:
+    GameManager.events_manager.event_started.connect(on_event_started)
+
+    GameManager.game_start()
+
+func on_event_started(event: Event):
+    user_prompt.text = event.text
+    sentence_begin.select(-1)
+    sentence_fill.select(-1)
+    sentence_end.select(-1)
