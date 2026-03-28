8505e3013f63018735b80eef69470589923dd7c9
KarolSiluch <siluchkarol@gmail.com>
2026-03-28 01:14:44 +0100

    new values

--- const_data/consts.gd
+++ const_data/consts.gd
@@ -3,18 +3,19 @@ class_name Consts
 const TYPE_SPEED = 1.0/90.0
 
 const BASE_EVENTS: Array[String] = [
-    "PAPIER_DO_KIBLA",
-    "FAKTURA",
-    "SAMOTNA_MATKA",
-    "EKSPRES_DO_KAWY",
-    "ZAPCHANY_KIBEL",
-    "ODKURZACZ_DO_DOMU",
-    "KAMPANIA_REKLAMOWA",
-    "FLIRT_Z_PRACOWNIKIEM",
-    "ZSZYWACZ_W_GALARETCE",
-    "WALIZKA",
-    "KAWA",
-    "WYWIADY",
-    "RANDKA1",
-    "NAGLOSNIENIE"
+    # "PAPIER_DO_KIBLA",
+    # "FAKTURA",
+    # "SAMOTNA_MATKA",
+    # "EKSPRES_DO_KAWY",
+    # "ZAPCHANY_KIBEL",
+    # "ODKURZACZ_DO_DOMU",
+    # "KAMPANIA_REKLAMOWA",
+    # "FLIRT_Z_PRACOWNIKIEM",
+    # "ZSZYWACZ_W_GALARETCE",
+    # "WALIZKA",
+    # "KAWA",
+    # "WYWIADY",
+    # "RANDKA1",
+    # "NAGLOSNIENIE",
+    "PAPIEROSY"
 ]
